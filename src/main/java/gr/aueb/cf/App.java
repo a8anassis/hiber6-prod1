136e169d1cc82fbf450027deb63549dd890501c0
Th. Androutsos <a8anassis@gmail.com>
2024-09-17 21:01:39 +0300

    updated with criteria api

--- src/main/java/gr/aueb/cf/App.java
+++ src/main/java/gr/aueb/cf/App.java
@@ -6,7 +6,9 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.*;
 
+import java.awt.image.CropImageFilter;
 import java.util.List;
 import java.util.Objects;
 
@@ -68,18 +70,86 @@ public class App
 //        List<Teacher> teacherWithNoCourse = query9.getResultList();
 
         // Select the most popular courses by teachers' count
-        String sql10 = "SELECT c.title, COUNT(t) FROM Course c JOIN c.teacher t GROUP BY c.title ORDER BY COUNT(t) DESC";
-        TypedQuery<Object[]> query10 = em.createQuery(sql10, Object[].class);
-        List<Object[]> popularCourses = query10.getResultList();
+//        String sql10 = "SELECT c.title, COUNT(t) FROM Course c JOIN c.teacher t GROUP BY c.title ORDER BY COUNT(t) DESC";
+//        TypedQuery<Object[]> query10 = em.createQuery(sql10, Object[].class);
+//        List<Object[]> popularCourses = query10.getResultList();
+
+        /**
+         * Criteria API.
+         *
+         * Better than JPQL for dynamic queries.
+         * Provides two main interfaces: 1) CriteriaBuilder, 2) CriteriaQuery<T>
+         *
+         * CriteriaBuilder provides API defining predicates (boolean expressions in WHERE clauses).
+         *
+         * CriteriaQuery<T> provides API  for creating queries, entity roots (FROM)
+         * returned results,and for adding criteria (WHERE)
+         *
+         */
+
+        // List courses
+//        CriteriaBuilder cb = em.getCriteriaBuilder();
+//        CriteriaQuery<String> query = cb.createQuery(String.class);
+//        Root<Course> course = query.from(Course.class);
+//        query.select(course.get("title"));
+//        List<String> titles = em.createQuery(query).getResultList();
+
+        // Select teachers with a specific firstname
+//        CriteriaBuilder cb = em.getCriteriaBuilder();
+//        CriteriaQuery<Teacher> query = cb.createQuery(Teacher.class);
+//        Root<Teacher> teacher = query.from(Teacher.class);
+//        ParameterExpression<String> lastnameParam = cb.parameter(String.class, "lastname");
+//        query.select(teacher).where(cb.equal(teacher.get("lastname"), lastnameParam));
+//        List<Teacher> teachers = em.createQuery(query).setParameter("lastname", "Ανδρούτσος").getResultList();
+
+        // Find courses taught by a specific teacher
+//        CriteriaBuilder cb = em.getCriteriaBuilder();
+//        CriteriaQuery<Course> query = cb.createQuery(Course.class);
+//        Root<Course> course = query.from(Course.class);
+//        Join<Course, Teacher> teacher = course.join("teacher");
+//
+//        ParameterExpression<String> lastnameParam = cb.parameter(String.class, "lastname");
+//        query.select(course).where(cb.equal(teacher.get("lastname"), lastnameParam));
+//        List<Course> courses = em.createQuery(query).setParameter("lastname", "Ανδρούτσος").getResultList();
+
+        // Teachers with more than one courses
+//        CriteriaBuilder cb = em.getCriteriaBuilder();
+//        CriteriaQuery<Teacher> query = cb.createQuery(Teacher.class);
+//        Root<Teacher> teacher = query.from(Teacher.class);
+//        query.select(teacher).where(cb.greaterThan(cb.size(teacher.get("courses")), 1));
+//        List<Teacher> teachers = em.createQuery(query).getResultList();
+
+        // get a list with courses title with teachers lastname firstname
+
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Object[]> query = cb.createQuery(Object[].class);
+        Root<Course> course = query.from(Course.class);
+        Join<Course, Teacher> teacher = course.join("teacher");
+        query.multiselect(course.get("title"), teacher.get("lastname"), teacher.get("firstname"));
+        List<Object[]> coursesTeachers = em.createQuery(query).getResultList();
 
         em.getTransaction().commit();
 
-        for (Object[] row : popularCourses) {
+        for (Object[] row : coursesTeachers) {
             for (Object item : row) {
                 System.out.print(item + " ");
             }
             System.out.println();
         }
+
+
+        //teachers.forEach(System.out::println);
+        //courses.forEach(System.out::println);
+
+        // teachers.forEach(System.out::println);
+        //titles.forEach(System.out::println);
+
+//        for (Object[] row : popularCourses) {
+//            for (Object item : row) {
+//                System.out.print(item + " ");
+//            }
+//            System.out.println();
+//        }
 
         //teacherWithNoCourse.forEach(System.out::println);
 //        for (Object[] row : teachersCoursesOrdered) {
